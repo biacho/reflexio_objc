9f8d6793ad2c6314cc4d59c675b470eab3b9e43f
Tobiasz Czelakowski <tobiasz@czelakowski.pl>
2013-03-25 16:40:39 +0100

    Zaimplementowanych ruch kulki

--- reflexio/ViewController.m
+++ reflexio/ViewController.m
@@ -14,6 +14,7 @@
 {
 	IBOutlet UIView *backgroundView;
 	UIView *ball, *tray;
+	double x,y,t;
 }
 
 @end
@@ -22,9 +23,13 @@
 #define TRAY_SIZE_X 80
 #define TRAY_SIZE_Y 20
 
+#define TIME 0.005
+
 @implementation ViewController
 
 // Create Section
+
+// !!!: KULKA
 - (void)createBall
 {
 	// Tutaj będzie tworzona piłeczka.
@@ -38,6 +43,73 @@
 	ball.opaque = NO;
 	[self.view addSubview:ball];
 }
+
+- (void)moveBall
+{
+	[UIView beginAnimations:nil context:nil];
+	[UIView setAnimationDuration:0.005];
+	[UIView setAnimationDelegate:self];
+	
+	//CGPoint ballPosition = CGPointMake(ball.frame.origin.x, ball.frame.origin.y);
+	CGPoint ballPosition = CGPointMake(ball.center.x, ball.center.y);
+	CGPoint buffor;
+	buffor = ballPosition;
+	
+	buffor.x += x;
+	buffor.y += y;
+	
+	//NSLog(@"y: %g", y);
+	//NSLog(@"buffor: %@ : %@", [NSNumber numberWithFloat:buffor.x], [NSNumber numberWithFloat:buffor.y]);
+		
+	if ([self przeszkoda:buffor])
+	{
+		[self sciana:buffor];
+		[self performSelector:@selector(moveBall) withObject:nil afterDelay:TIME];
+	}
+	else
+	{
+		ball.center = buffor;
+		[self performSelector:@selector(moveBall) withObject:nil afterDelay:TIME];
+	}
+	
+	[UIView commitAnimations];
+}
+
+- (BOOL)przeszkoda:(CGPoint)point
+{
+	if (x != 0 || y != 0)
+	{
+		if (point.x /*+ BALL_SIZE/2*/ >= self.view.bounds.size.width ||point.x /*- BALL_SIZE/2*/ <= 0) return YES;
+		else if (point.y /*+ BALL_SIZE/2*/ >= self.view.bounds.size.height || point.y /*- BALL_SIZE/2*/ <= 0) return YES;
+		else return NO;
+	}
+	return YES;
+}
+
+- (void)sciana:(CGPoint)point
+{
+	if (point.y >= self.view.bounds.size.height || point.y <= 0) // Dolna ściana && Górna ściana
+	{
+		if (point.y <= 0)
+		{
+			//x = x;
+			y = -y;
+		}
+		else
+		{
+			y = -y;
+			NSLog(@"Dolna Ściana...");
+		}
+	}
+	else if (point.x >= self.view.bounds.size.width || point.x <= 0) // Prawa ściana && Lewa ściana
+	{
+		x = -x;
+		//y = y;
+	}
+
+}
+
+// !!!: TACKA
 - (void)createTray
 {
 	// Tutaj będzie tworzona tacka.
@@ -55,28 +127,29 @@
 	UIPanGestureRecognizer *moveTrayGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveTray:)];
     [tray addGestureRecognizer:moveTrayGesture];
 }
-
 - (void)moveTray:(UIPanGestureRecognizer *)move
 {
 	CGPoint translation = [move translationInView:self.view];
-	CGPoint temp = CGPointMake(tray.frame.origin.x, tray.center.y);
+	CGPoint trayPosition = CGPointMake(tray.frame.origin.x, tray.center.y);
 	
-	temp.x += translation.x;
-	if (temp.x <= 0)
+	trayPosition.x += translation.x;
+	if (trayPosition.x <= 0)
 	{
-		temp.x = 0;
+		trayPosition.x = 0;
 		NSLog(@"Lewa ściana.");
 	}
-	else if (temp.x >= self.view.frame.size.width - TRAY_SIZE_X)
+	else if (trayPosition.x >= self.view.frame.size.width - TRAY_SIZE_X)
 	{
-		temp.x = self.view.frame.size.width - TRAY_SIZE_X;
+		trayPosition.x = self.view.frame.size.width - TRAY_SIZE_X;
 		NSLog(@"Prawa ściana.");
 	}
 	
-	tray.center = CGPointMake(temp.x + TRAY_SIZE_X/2, temp.y);
-	NSLog(@"Pozycja Tacki: %@", [NSNumber numberWithFloat:temp.x]);
+	tray.center = CGPointMake(trayPosition.x + TRAY_SIZE_X/2, trayPosition.y);
+	//NSLog(@"Pozycja Tacki: %@", [NSNumber numberWithFloat:temp.x]);
 	[move setTranslation:CGPointZero inView:self.view];
 }
+
+
 
 // Set Section
 - (void)gameOver
@@ -97,7 +170,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-	NSLog(@"WITAJ!");
+	NSLog(@"reFlexio!");
 	
 	// Inicjalizacja
 	[self createBall];
@@ -106,6 +179,11 @@
 	
 	// DEBUG
 	//[self gameOver];
+	x = 0;
+	y = 1;
+	t = TIME;
+	[self moveBall];
+	// -----
 }
 
 - (void)didReceiveMemoryWarning
