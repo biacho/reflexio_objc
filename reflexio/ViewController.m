c7e0a2ee130885433c25fe607c6e5aaf81c87e23
Tobiasz Czelakowski <tobiasz@czelakowski.pl>
2013-03-25 14:44:49 +0100

    Zaimplementowany ruch tacki

--- reflexio/ViewController.m
+++ reflexio/ViewController.m
@@ -12,14 +12,14 @@
 
 @interface ViewController ()
 {
-	//UIView *backgroundView;
 	IBOutlet UIView *backgroundView;
+	UIView *ball, *tray;
 }
 
 @end
 
 #define BALL_SIZE 20
-#define TRAY_SIZE_X 70
+#define TRAY_SIZE_X 80
 #define TRAY_SIZE_Y 20
 
 @implementation ViewController
@@ -29,8 +29,12 @@
 {
 	// Tutaj będzie tworzona piłeczka.
 	NSLog(@"I'm creating a Ball...");
-	CGRect ballFrame = CGRectMake(self.view.center.x, self.view.center.y, BALL_SIZE, BALL_SIZE);
-	UIView *ball = [[BallView alloc] initWithFrame:ballFrame];
+	CGRect ballFrame = CGRectMake(self.view.center.x - BALL_SIZE/2,
+								  self.view.center.y - BALL_SIZE/2,
+								  BALL_SIZE,
+								  BALL_SIZE);
+	
+	ball = [[BallView alloc] initWithFrame:ballFrame];
 	ball.opaque = NO;
 	[self.view addSubview:ball];
 }
@@ -38,10 +42,40 @@
 {
 	// Tutaj będzie tworzona tacka.
 	NSLog(@"I'm creating a Tray...");
-	CGRect trayFrame = CGRectMake(self.view.center.x, self.view.bounds.size.height - 50, TRAY_SIZE_X, TRAY_SIZE_Y);
-	UIView *tray = [[TrayView alloc] initWithFrame:trayFrame];
+	CGRect trayFrame = CGRectMake(self.view.center.x - TRAY_SIZE_X/2,
+								  self.view.bounds.size.height - 50,
+								  TRAY_SIZE_X,
+								  TRAY_SIZE_Y);
+	
+	tray = [[TrayView alloc] initWithFrame:trayFrame];
 	tray.opaque = NO;
 	[self.view addSubview:tray];
+	
+	// gest
+	UIPanGestureRecognizer *moveTrayGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveTray:)];
+    [tray addGestureRecognizer:moveTrayGesture];
+}
+
+- (void)moveTray:(UIPanGestureRecognizer *)move
+{
+	CGPoint translation = [move translationInView:self.view];
+	CGPoint temp = CGPointMake(tray.frame.origin.x, tray.center.y);
+	
+	temp.x += translation.x;
+	if (temp.x <= 0)
+	{
+		temp.x = 0;
+		NSLog(@"Lewa ściana.");
+	}
+	else if (temp.x >= self.view.frame.size.width - TRAY_SIZE_X)
+	{
+		temp.x = self.view.frame.size.width - TRAY_SIZE_X;
+		NSLog(@"Prawa ściana.");
+	}
+	
+	tray.center = CGPointMake(temp.x + TRAY_SIZE_X/2, temp.y);
+	NSLog(@"Pozycja Tacki: %@", [NSNumber numberWithFloat:temp.x]);
+	[move setTranslation:CGPointZero inView:self.view];
 }
 
 // Set Section
